0af11b1b65945b428fc5a0a0ae66e9eba225608f
David Millan <hello@davidmillan.me>
2019-09-19 14:16:01 +0100

    Completed S5.72 Coding Challenge 7

--- script.js
+++ script.js
@@ -372,6 +372,66 @@ c) correct answer (I would use a number for this)
 
 // IIFE for privacy
 
+// (function() {
+//     // Function Constructor
+//     function Question(question, answers, correct) {
+//         this.question = question;
+//         this.answers = answers;
+//         this.correct = correct;
+//     }
+
+//     // Write the method on Question prototype property
+//     // Prototype chain to not have to copy the method
+//     // on all questions
+//     Question.prototype.displayQuestion = function() {
+//         console.log(this.question);
+
+//         for (var i = 0; i < this.answers.length; i++) {
+//             console.log(i + ': ' + this.answers[i]);
+//         }
+//     }
+
+//     Question.prototype.checkAnswer = function(ans) {
+//         if (ans === this.correct) {
+//             console.log('Correct answer!');
+//         } else {
+//             console.log('Wrong answer! Try again :)');
+//         }
+//     }
+
+//     var q1 = new Question('Is JS the coolest programming language in the world?', ['Yes', 'No'], 0);
+
+//     var q2 = new Question('What\'s the name of this course\'s teacher?', ['John', 'Michael', 'Jonas'], 2);
+
+//     var q3 = new Question('What does best describe coding?', ['Boring', 'Hard', 'Fun', 'Tediuos'], 2);
+
+//     //Retrive random question
+//     var questions = [q1, q2, q3];
+
+//     var n = Math.floor(Math.random() * questions.length);
+
+//     questions[n].displayQuestion();
+
+//     // Alert with text capabilities
+//     var answer = parseInt(prompt('Please select the correct answer.'));
+
+//     questions[n].checkAnswer(answer);
+// })();
+
+/*
+--- Expert level ---
+
+8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+
+9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+
+10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
+
+11. Display the score in the console. Use yet another method for this.
+*/
+
+// IIFE for privacy
+
 (function() {
     // Function Constructor
     function Question(question, answers, correct) {
@@ -391,13 +451,25 @@ c) correct answer (I would use a number for this)
         }
     }
 
-    Question.prototype.checkAnswer = function(ans) {
+    Question.prototype.checkAnswer = function(ans, callback) {
+        var sc;
         if (ans === this.correct) {
             console.log('Correct answer!');
+            sc = callback(true);
         } else {
             console.log('Wrong answer! Try again :)');
+
+            sc = callback(false);
         }
+
+        this.displayScore(sc);
     }
+
+    Question.prototype.displayScore = function(score) {
+        console.log('Your current score is: ' + score);
+        console.log('Type in \'exit\' to close prompt');
+        console.log('--------------------------------')
+    } 
 
     var q1 = new Question('Is JS the coolest programming language in the world?', ['Yes', 'No'], 0);
 
@@ -405,15 +477,38 @@ c) correct answer (I would use a number for this)
 
     var q3 = new Question('What does best describe coding?', ['Boring', 'Hard', 'Fun', 'Tediuos'], 2);
 
-    //Retrive random question
     var questions = [q1, q2, q3];
 
-    var n = Math.floor(Math.random() * questions.length);
+    function score() {
+        var sc = 0;
+        return function(correct) {
+            if (correct) {
+                sc++;
+            }
+            return sc;
+        }
+    }
 
-    questions[n].displayQuestion();
+    var keepScore = score();
 
-    // Alert with text capabilities
-    var answer = parseInt(prompt('Please select the correct answer.'));
+    function nextQuestion() {
 
-    questions[n].checkAnswer(answer);
+        //Retrive random question
+        var n = Math.floor(Math.random() * questions.length);
+
+        questions[n].displayQuestion();
+
+        // Alert with text capabilities
+        var answer = prompt('Please select the correct answer.');
+
+        if(answer !== 'exit') {
+            questions[n].checkAnswer(parseInt(answer), keepScore);
+
+            nextQuestion();
+        };
+
+    }
+
+    nextQuestion();
+
 })();
